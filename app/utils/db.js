a68c5812f8c0dbe72ed19deacf974d4e01abb9cd
hartikteetwal <hartik.teetwal@ezbizsoft.in>
2026-01-31 17:30:10 +0530

    changes

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const connectionStr = `mongodb+srv://teetwalhartik:hartik123456@cluster0.yuzgqpf.mongodb.net/qr_maker?retryWrites=true&w=majority&appName=Cluster0`
+const connectionStr = process.env.MONGODB_URI 
 
 export const connectDB = async () => {
     await mongoose.connect(connectionStr)
